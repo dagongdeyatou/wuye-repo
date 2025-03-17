55710a2c779590596a80d56cb09b6f4036ba5329
wuye <wuye.wzy@alibaba-inc.com>
2025-03-17 15:20:51 +0800

    study

--- type_tip_demo.py
+++ type_tip_demo.py
@@ -4,10 +4,19 @@
 # @Author  : wuye
 # @File    : type_tip_demo.py
 # @Software: PyCharm
+from __future__ import annotations
 
 def add(a: int, b: int) -> int:
     return a + b
 
+class MyClass:
+    def copy(self) -> MyClass:  # 即使 MyClass 尚未完全定义，也不会报错
+        return MyClass()
+
+
 
 if __name__ == '__main__':
     print(add(1,2))
+    myclass = MyClass()
+    myclass.copy()
+    print(myclass.copy.__annotations__)
